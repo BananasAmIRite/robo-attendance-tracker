93a288d195ea37c6eaf6dc49d4ee9aff8a28186f
Vincent Zheng <vincentzheng1423@gmail.com>
2024-06-07 09:23:52 -0400

    add untested oauth2 credential & service creation

--- modules/google-sheets-query/android/src/main/java/com/googlesheetsquery/GoogleSheetsQueryModule.java
+++ modules/google-sheets-query/android/src/main/java/com/googlesheetsquery/GoogleSheetsQueryModule.java
@@ -1,9 +1,21 @@
 package com.googlesheetsquery;
 
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
-import android.os.Bundle;
+import com.facebook.react.bridge.ActivityEventListener;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.sheets.v4.Sheets;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -12,19 +24,45 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.module.annotations.ReactModule;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
 
 
 @ReactModule(name = GoogleSheetsQueryModule.NAME)
 public class GoogleSheetsQueryModule extends ReactContextBaseJavaModule {
   public static final String NAME = "GoogleSheetsQuery";
 
+  private static final String PREF_ACCOUNT_NAME = "????";
+  private static final int REQUEST_ACCOUNT_PICKER = 420;
+
+    private GoogleAccountCredential credential;
+  private Sheets service;
 
   public GoogleSheetsQueryModule(ReactApplicationContext reactContext)  {
     super(reactContext);
+    reactContext.addActivityEventListener(new ActivityEventListener() {
+      @Override
+      public void onActivityResult(Activity activity, int requestCode, int resultCode, @Nullable Intent data) {
+          if (resultCode != REQUEST_ACCOUNT_PICKER || resultCode != Activity.RESULT_OK || data == null || data.getExtras() == null) return;
+
+          final String accountName = data.getExtras().getString(AccountManager.KEY_ACCOUNT_NAME);
+
+          if (accountName == null) return;
+
+          credential.setSelectedAccountName(accountName);
+          final SharedPreferences prefs = getCurrentActivity().getPreferences(Context.MODE_PRIVATE);
+          prefs.edit().putString(PREF_ACCOUNT_NAME, accountName).commit();
+      }
+
+      @Override
+      public void onNewIntent(Intent intent) {}
+    });
   }
 
   @Override
@@ -34,21 +72,40 @@ public class GoogleSheetsQueryModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-  public void getUserInformation(Promise promise) {
+  public void getUserInformation(Promise promise) throws GeneralSecurityException, IOException {
     // run at beginning of application to retrieve information about user if applicable, can return null
     // used for automatic sign in when user has signed in before
     // bundle with the access token (see modules/google-sheets-query/src/index.tsx)
     // alternatively, you can probably emit an event (google how to) and catch it in react if you want a way to
     // funnel data from two diff sources (signIn & getUserInformation)
-    promise.resolve(null);
+    this.credential = GoogleAccountCredential.usingOAuth2(getCurrentActivity(), Collections.singleton(SheetsScopes.SPREADSHEETS));
+
+    final SharedPreferences prefs = Objects.requireNonNull(getCurrentActivity()).getPreferences(Context.MODE_PRIVATE);
+
+    NetHttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+
+    credential.setSelectedAccountName(prefs.getString(PREF_ACCOUNT_NAME, null));
+
+    this.service = new Sheets.Builder(transport, GsonFactory.getDefaultInstance(), credential).setApplicationName("Attendance Tracker").build();
+
+    if (this.credential.getSelectedAccountName() != null) {
+      promise.resolve(this.credential.getToken());
+    } else {
+      promise.resolve(null);
+    }
   }
 
   @ReactMethod
-  public void signIn(Promise promise) {
+  public void signIn(Promise promise) throws GeneralSecurityException, IOException {
     // will run when user presses sign in button, probably will return user information same as getUserInformation
     // bundle with the access token (see modules/google-sheets-query/src/index.tsx)
-    promise.resolve(Arguments.createMap());
-    
+    Objects.requireNonNull(getCurrentActivity()).startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+
+    if (this.credential.getSelectedAccountName() != null) {
+      promise.resolve(this.credential.getToken());
+    } else {
+      promise.resolve(null);
+    }
   }
 
   @ReactMethod
--- modules/google-sheets-query/src/index.tsx
+++ modules/google-sheets-query/src/index.tsx
@@ -1,68 +1,80 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from "react-native";
 
 const LINKING_ERROR =
-    `The package 'react-native-google-sheets-query' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo Go\n';
+  `The package 'react-native-google-sheets-query' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
+  "- You rebuilt the app after installing the package\n" +
+  "- You are not using Expo Go\n";
 
 const GoogleSheetsQuery = NativeModules.GoogleSheetsQuery
-    ? NativeModules.GoogleSheetsQuery
-    : new Proxy(
-          {},
-          {
-              get() {
-                  throw new Error(LINKING_ERROR);
-              },
-          }
-      );
-
-export interface UserInformation {
-    accessToken: string;
-}
+  ? NativeModules.GoogleSheetsQuery
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
 
 export interface StudentInfo {
-    studentId: string;
-    studentName: string;
+  studentId: string;
+  studentName: string;
 }
 
 export interface AttendanceEntry {
-    studentId: string;
-    timestamp: string;
+  studentId: string;
+  timestamp: string;
 }
 
-export function getUserInformation(): Promise<UserInformation> {
-    return GoogleSheetsQuery.getUserInformation();
+export function getUserInformation(): Promise<string | null> {
+  return GoogleSheetsQuery.getUserInformation();
 }
 
-export function signIn(): Promise<UserInformation> {
-    return GoogleSheetsQuery.signIn();
+export function signIn(): Promise<string | null> {
+  return GoogleSheetsQuery.signIn();
 }
 
 export function postAttendanceEntry(
-    accessToken: string,
-    sheetId: string,
-    sheetRange: string,
-    studentId: string,
-    dateTime: string
+  accessToken: string,
+  sheetId: string,
+  sheetRange: string,
+  studentId: string,
+  dateTime: string
 ): Promise<boolean> {
-    return GoogleSheetsQuery.postAttendanceEntry(accessToken, sheetId, sheetRange, studentId, dateTime);
+  return GoogleSheetsQuery.postAttendanceEntry(
+    accessToken,
+    sheetId,
+    sheetRange,
+    studentId,
+    dateTime
+  );
 }
 
 export function getDailyAttendanceEntry(
-    accessToken: string,
-    sheetId: string,
-    sheetRange: string,
-    studentId: string
+  accessToken: string,
+  sheetId: string,
+  sheetRange: string,
+  studentId: string
 ): Promise<{ entries: AttendanceEntry[] }> {
-    return GoogleSheetsQuery.getDailyAttendanceEntry(accessToken, sheetId, sheetRange, studentId);
+  return GoogleSheetsQuery.getDailyAttendanceEntry(
+    accessToken,
+    sheetId,
+    sheetRange,
+    studentId
+  );
 }
 
 export function getStudentInfo(
-    accessToken: string,
-    sheetId: string,
-    sheetRange: string,
-    studentId: string
+  accessToken: string,
+  sheetId: string,
+  sheetRange: string,
+  studentId: string
 ): Promise<StudentInfo> {
-    return GoogleSheetsQuery.getStudentInfo(accessToken, sheetId, sheetRange, studentId);
+  return GoogleSheetsQuery.getStudentInfo(
+    accessToken,
+    sheetId,
+    sheetRange,
+    studentId
+  );
 }
